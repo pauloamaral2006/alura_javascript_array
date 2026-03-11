1f52a06c736a66b692991b50da79743febbf09e9
Paulo Amaral <pauloamaral2006@yahoo.com.br>
2026-03-11 16:03:50 -0300

    Aula 01

---
+++ removendo-nota.js
@@ -0,0 +1,8 @@
+const notas = [10, 6, 8, 5.5, 108];
+
+notas.pop();
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(`A média é ${media}`);
+console.log(notas);
